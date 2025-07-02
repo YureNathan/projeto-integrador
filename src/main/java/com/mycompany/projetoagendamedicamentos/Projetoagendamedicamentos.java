f8af168a99fe0307bfd8215f8efda9788aeb8ea3
Yure <yure.nathan303@gmail.com>
2025-07-02 11:27:00 -0300

    Inicializando o repositorio

---
+++ src/main/java/com/mycompany/projetoagendamedicamentos/Projetoagendamedicamentos.java
@@ -0,0 +1,8 @@
+package com.mycompany.projetoagendamedicamentos;
+
+public class Projetoagendamedicamentos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
